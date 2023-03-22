27db2f91530451eafa2041bb432d0ec06ba08bf5
Rapirys <ilyarotan@gmail.com>
2023-03-22 20:23:29 +0200

    Command.java

--- src/main/java/Command.java
+++ src/main/java/Command.java
@@ -16,6 +16,7 @@ public class Command {
         map.put("Who", (byte) 1);
         map.put("Exit", (byte) 0);
         map.put("AddPolygon", (byte) 100);
+        // test branch cahnges
     }
     private byte length;
 //    private List<Byte> commands;
