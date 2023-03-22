15010078e416f2203e79cff273e6d4717ea32c77
Rapirys <ilyarotan@gmail.com>
2023-03-22 18:08:23 +0200

    Client.java

--- src/main/java/Client.java
+++ src/main/java/Client.java
@@ -6,7 +6,7 @@ import java.util.logging.Logger;
 import java.util.logging.SimpleFormatter;
 
 public class Client {
-    //commit to test branch
+    //commit to test branch commit to master
 
     Logger logger = Logger.getLogger(Client.class.getName());
 
